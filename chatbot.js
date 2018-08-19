79110546971f4fbe25a1e9f3fba8bd8c4fa49236
Ivar Lee <ivar@hackerspring.com>
2018-08-19 03:59:40 -0700

    initial commit

---
+++ chatbot.js
@@ -0,0 +1,159 @@
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const { RichEmbed } = require('discord.js')
+const requestPlayerInfo = require('./request-info')
+
+app.get('/', (req, res) => {
+    res.send(':D')
+})
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
+})
+
+client.on('message', async msg => {
+
+    const pubgQueryFPP = msg.content.indexOf('!pubg') === 0 ? true : false
+    const pubgQueryTPP = msg.content.indexOf('!TPPpubg') === 0 ? true : false
+
+    if (msg.content.toLowerCase() === "help") {
+        msg.reply('Type "!pubg:[Your PUBG name]" to see some FPP statistics. Example: !pubg:Rip_Torn')
+        msg.reply('Type "!TPPpubg:[Your PUBG name]" to see some TPP statistics. Example: !TPPpubg:Rip_Torn')
+        msg.reply('Type !commands to see other commands.')
+    }
+    
+    if (pubgQueryFPP) {        
+        const name = msg.content.split(':')[1]
+        let playerInfo
+        try {
+            if (name) {
+                playerInfo = await requestPlayerInfo(name)
+                const message = (
+                `
+                
+                -----Last Match-----
+                Match Type: ${playerInfo.lastMatch.gameType}
+                Placed: ${playerInfo.lastMatch.winPlace}
+                Longest Kill: ${playerInfo.lastMatch.longestKill} meters
+                Kills: ${playerInfo.lastMatch.kills}
+                Headshot Kills: ${playerInfo.lastMatch.headshotKills}
+                Kill assists: ${playerInfo.lastMatch.assists}
+                Revived teammates: ${playerInfo.lastMatch.revives} times
+                Was knocked down: ${playerInfo.lastMatch.knocks} times
+                
+                -----Wins-----
+                Solo: ${playerInfo['solo-fpp'].wins} ==> ${playerInfo['solo-fpp'].ratio} win rate
+                Duo: ${playerInfo['duo-fpp'].wins} ==> ${playerInfo['duo-fpp'].ratio} win rate 
+                Squad: ${playerInfo['squad-fpp'].wins} ==> ${playerInfo['squad-fpp'].ratio} win rate
+                
+                -----Longest Kill-----
+                Solo: ${playerInfo.longestKills['solo-fpp']}
+                Duo: ${playerInfo.longestKills['duo-fpp']}
+                Squad: ${playerInfo.longestKills['squad-fpp']}
+                
+                -----Knocks-----
+                Solo: ${playerInfo.totalKnocks['solo-fpp']}
+                Duo: ${playerInfo.totalKnocks['duo-fpp']}
+                Squad: ${playerInfo.totalKnocks['squad-fpp']}
+
+                `
+                )
+
+                const embed = new RichEmbed()
+                .setTitle(`${name}'s FPP Stats (ONLY CURRENT SEASON)`)
+                .setColor(0xFF0000)
+                .setDescription(message)
+                msg.channel.send(embed)
+
+            } else {
+                msg.reply('Did you enter the player name correctly?')
+            }
+        } catch(error) {
+            msg.reply(error)
+        }
+    }
+
+    if (pubgQueryTPP) {
+        const name = msg.content.split(':')[1]
+        let playerInfo
+        try {
+            if (name) {
+                playerInfo = await requestPlayerInfo(name)
+                const message = (
+                `
+               
+                -----Last Match -----
+                Match Type: ${playerInfo.lastMatch.gameType}
+                Placed: ${playerInfo.lastMatch.winPlace}
+                Longest Kill: ${playerInfo.lastMatch.longestKill} meters
+                Kills: ${playerInfo.lastMatch.kills}
+                Headshot Kills: ${playerInfo.lastMatch.headshotKills}
+                Kill assists: ${playerInfo.lastMatch.assists}
+                Revived teammates: ${playerInfo.lastMatch.revives} times
+                Was knocked down: ${playerInfo.lastMatch.knocks} times
+
+                -----Wins -----
+                Solo: ${playerInfo.solo.wins} ==> ${playerInfo.solo.ratio} win rate
+                Duo: ${playerInfo.duo.wins} ==> ${playerInfo.duo.ratio} win rate
+                Squad: ${playerInfo.squad.wins} ==> ${playerInfo.squad.ratio} win rate
+                
+                -----Longest Kill-----
+                Solo: ${playerInfo.longestKills.solo}
+                Duo: ${playerInfo.longestKills.duo}
+                Squad: ${playerInfo.longestKills.squad}      
+               
+                -----Knocks -----
+                Solo: ${playerInfo.totalKnocks.solo}
+                Duo: ${playerInfo.totalKnocks.duo}
+                Squad: ${playerInfo.totalKnocks.squad}
+                
+                `
+                )
+                const embed = new RichEmbed()
+                .setTitle(`${name}'s TPP Stats (ONLY CURRENT SEASON)`)
+                .setColor(0xFF0000)
+                .setDescription(message)
+                msg.channel.send(embed)
+            } else {
+                msg.reply('Did you enter the player name correctly?')
+            }
+        } catch(error) {
+            msg.reply(error)
+        }       
+    }
+
+    if (msg.content.toLowerCase() === '!commands') {
+        msg.reply(
+        `
+        Commands
+        - "brb" plays a sound in your current voice channel
+        - "monster kill" plays a sound in your current voice channel
+        `
+        )
+    }
+    
+    if (msg.content.toLowerCase() === 'brb' && msg.member.voiceChannel) {
+        const connection = await msg.member.voiceChannel.join()
+        connection.playFile('./illbeback.mp3')
+    } else if (msg.content.toLowerCase() === 'brb') {
+        msg.reply('See you later!')
+    }
+
+    if (msg.content.toLowerCase() === 'monster kill') {
+        const connection = await msg.member.voiceChannel.join()
+        connection.playFile('./monsterkill.mp3')
+    } else if (msg.content.toLowerCase() === 'monster kill') {
+        console.log('user not in voice channel (m kill)')
+        msg.reply('M-M-M-M MONSTER KILLLLL')
+    }
+
+})
+
+client.login(process.env.BOT_TOKEN)
+app.listen(PORT, () => console.log(`express server listening on port: ${PORT}`))
---
+++ request-info.js
@@ -0,0 +1,181 @@
+const request = require('request')
+
+module.exports = async name => {
+
+    console.log(`${name} is requesting their PUBG stats.`)
+
+    const options = {
+        url: `https://api.pubg.com/shards/pc-na/players?filter[playerNames]=${name}`,
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkZjliNmE4MC03ZTQxLTAxMzYtOWYzOS0zZjI5OTY0ZTI5MjQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTMzODQ3MDEzLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImJicV9ib3QifQ.QjZIr5RbD2WyVtK0saSqD-oPxuU6FM3ZR3a8OQcqluM',
+            'Accept': 'application/vnd.api+json'
+        }
+    }
+
+    let lastMatchId = null
+
+    const playerId = await new Promise((resolve, reject) => {
+        request(options, (error, res, body) => {
+            if (!error && res.statusCode == 200) {
+                const info = JSON.parse(body)
+                lastMatchId = info.data[0].relationships.matches.data[0].id || null
+                const playerId = info.data[0].id
+                resolve(playerId)
+            } else {
+                reject(' I could not find a user by that name in the NA region.')
+            }
+        })
+    })
+
+    options.url = 'https://api.pubg.com/shards/pc-na/seasons'
+
+    const currentSeason = await new Promise((resolve, reject) => {
+        request(options, (error, res, body) => {
+            if (!error && res.statusCode == 200) {
+                let currentSeason = ''
+                const seasons = JSON.parse(body)
+                for (const season of seasons.data) {
+                    if (season.attributes.isCurrentSeason) currentSeason = season.id
+                }                
+                resolve(currentSeason)
+            } else {
+                reject(' there was an error finding the current season.')
+            }
+        })
+    })
+
+    options.url = `https://api.pubg.com/shards/pc-na/players/${playerId}/seasons/${currentSeason}`
+
+    // player info
+    
+    let playerInfo = {
+        lastMatchId,
+        region: 'North America PC (pc-na)',
+        username: name,
+        season: currentSeason,
+        solo: {
+            wins: 0,
+            losses: 0,
+            ratio: 0
+        },
+        'solo-fpp': {
+            wins: 0,
+            losses: 0,
+            ratio: 0
+        },
+        duo: {
+            wins: 0,
+            losses: 0,
+            ratio: 0
+        },
+        'duo-fpp': {
+            wins: 0,
+            losses: 0,
+            ratio: 0
+        },
+        squad: {
+            wins: 0,
+            losses: 0,
+            ratio: 0
+        },
+        'squad-fpp': {
+            wins: 0,
+            losses: 0,
+            ratio: 0
+        },
+        longestKills: {
+            solo: 0,
+            'solo-fpp': 0,
+            duo: 0,
+            'duo-fpp': 0,
+            squad: 0,
+            'squad-fpp': 0
+        },
+        totalKnocks: {
+            solo: 0,
+            'solo-fpp': 0,
+            duo: 0,
+            'duo-fpp': 0,
+            squad: 0,
+            'squad-fpp': 0
+        },
+        lastMatch: {
+            winPlace: 0,
+            longestKill: 0,
+            kills: 0,
+            assists: 0,
+            revives: 0,
+            knocks: 0,
+            gameType: ''
+        }
+    }
+
+    const playerData = await new Promise((resolve, reject) => {
+        request(options, (error, res, body) => {
+            if (!error && res.statusCode == 200) {
+                const info = JSON.parse(body)
+                const stats = info.data.attributes.gameModeStats
+                for (const gametype in stats) {
+                    if (gametype in playerInfo) {
+                        for (const statistic in stats[gametype]) {
+                            if (statistic === 'wins') {
+                                playerInfo[gametype].wins = stats[gametype].wins
+                            }
+                            if (statistic === 'losses') {
+                                playerInfo[gametype].losses = stats[gametype].losses
+                            }
+                            const ratio = (playerInfo[gametype].wins/playerInfo[gametype].losses) || 0
+                            const percent = Math.round(ratio * 100)
+                            playerInfo[gametype].ratio = `${percent}%`
+                            if (statistic === 'longestKill') {
+                                const longKill = Math.round(stats[gametype].longestKill * 100)/100
+                                playerInfo.longestKills[gametype] = `${longKill} meters`
+                            }
+                            if (statistic === 'dBNOs') playerInfo.totalKnocks[gametype] = `${stats[gametype].dBNOs} enemies knocked down`
+                            if (statistic === 'losses') playerInfo.totalKnocks[gametype] = `${stats[gametype].dBNOs} enemies knocked down`
+                        }
+                    }
+                }
+                resolve(playerInfo)
+            } else {
+                reject(' I was unable to find any data related to that player name on the NA region.')
+            }
+        })
+    })
+
+    // get last match 
+    
+    options.url = `https://api.pubg.com/shards/pc-na/matches/${playerInfo.lastMatchId}`
+
+    const lastMatchData = await new Promise((resolve, reject) => {
+        request(options, (error, res, body) => {
+            if (!error && res.statusCode == 200) {
+                const info = JSON.parse(body)
+                info.included.forEach(val => {
+                    if ((val.type === 'participant') && (name === val.attributes.stats.name)) {
+                        playerInfo.lastMatch.winPlace = val.attributes.stats.winPlace
+                        const longKill = Math.round(val.attributes.stats.longestKill * 100)/100
+                        playerInfo.lastMatch.longestKill = longKill
+                        playerInfo.lastMatch.kills = val.attributes.stats.kills
+                        playerInfo.lastMatch.headshotKills = val.attributes.stats.headshotKills
+                        playerInfo.lastMatch.assists = val.attributes.stats.assists
+                        playerInfo.lastMatch.revives = val.attributes.stats.revives
+                        playerInfo.lastMatch.knocks = val.attributes.stats.DBNOs
+                        playerInfo.lastMatch.gameType = info.data.attributes.gameMode
+                    }
+                })
+                resolve(playerInfo)
+            } else {
+                reject(' when checking for your last played match I was unable to find any data for the NA region.')
+            }
+        })
+    })
+
+    playerInfo.currentSeason = currentSeason
+    
+    console.log(playerInfo)
+
+    return playerInfo
+
+}

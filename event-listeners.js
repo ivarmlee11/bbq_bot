6e805799d18e6813f8978cb54d7d7131e53f63cd
Ivar Lee <ivar@londontrustmedia.com>
2018-11-03 19:08:13 -0700

    show errors

--- event-listeners.js
+++ event-listeners.js
@@ -89,6 +89,7 @@ module.exports = {
 
                 }
             } catch(error) {
+                console.log(error)
                 msg.reply(error)
             }
         }
@@ -157,6 +158,7 @@ module.exports = {
                 }
             } catch(error) {
                 console.log(error)
+                msg.reply(error)
             }       
         }
     }
